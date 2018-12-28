fc04637b12692130fc4f315274bc217f03261e06
caigx-pc <caiguo0421@163.com>
2018-12-28 09:31:22 +0800

    增加Login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+// 引入vuetify的css
+import 'vuetify/dist/vuetify.min.css'
+// 引入Material Design图标
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+// 安装babel支持IE11和Safari 9
+import 'babel-polyfill'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
